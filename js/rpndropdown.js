f0c18f3cb7bcb457ead06f1cc2efcf1f6bcce790
roquierm <michel.roquier@rpn.ch>
2015-11-27 13:39:10 +0000

    merge master and dev codes / add alternative solution and image display functionality

--- js/rpndropdown.js
+++ js/rpndropdown.js
@@ -19,24 +19,22 @@ var rpndropdownmodule = function() {
         }else{
             state= _.map(_.filter(datas.items,function(item){return item.choice.length>1}),function(item,idx){return '';});
         }
-        
         buildUi();
     };
 
     var buildUi = function() {
-        domelem.addClass('dropdown');
+        var uicontents = $('<div class="form-inline">');
 
         //build panel with sentence
-        if(!_.isEmpty(datas.circumstance[0])) {domelem.append($('<hr style="border-top: 5px solid #ccc;"><p><b>' + datas.circumstance[0] + '</b></p>'));}
-		if(!_.isEmpty(datas.sentence)) {domelem.append($('<p>' + datas.sentence + '</p>'));}
-        if(!_.isEmpty(datas.circumstance[1])) {domelem.append($('<hr style="border-top: 5px solid #ccc;"><p><b>' + datas.circumstance[1] + '</b></p>'));}
+        if(!_.isEmpty(datas.circumstance[0])) {uicontents.append($('<hr style="border-top: 5px solid #ccc;"><p><b>' + datas.circumstance[0] + '</b></p>'));}
+		if(!_.isEmpty(datas.sentence)) {uicontents.append($('<p>' + datas.sentence + '</p>'));}
+        if(!_.isEmpty(datas.circumstance[1])) {uicontents.append($('<hr style="border-top: 5px solid #ccc;"><p><b>' + datas.circumstance[1] + '</b></p>'));}
 
         //build sentence with items to select
-        var sentenceToComplete=$('<div class="form-inline">');
         var internalCounter=0;
         $.each(datas.items, function(idx, item) {
             if(item.choice.length==1){
-                sentenceToComplete.append(" " + item.choice[0] + " ");
+                uicontents.append(" " + item.choice[0] + " ");
             }else{
                 var opts=[];
                 
@@ -44,12 +42,28 @@ var rpndropdownmodule = function() {
                 $.each(datas.items[idx].choice, function(id, choice){
                     opts[id+1]=$('<option value="' + choice+ '" '+(state[internalCounter]==choice?'selected':'')+'>' + choice + '</option>');
                 });
-                sentenceToComplete.append($('<select class="rpnm-input dropdown form-control">').append(opts));
+                uicontents.append($('<select class="rpnm-input dropdown form-control">').append(opts));
                 internalCounter++;
             }
         });
-        domelem.append(sentenceToComplete);
-
+        if(!_.isUndefined(datas.illustration)){
+        	_.defaults(datas.illustration,{
+        		position:"top",
+        		url:"<img />"
+        	});
+        	var illus=$(datas.illustration.url).addClass('img-rounded');
+        	if(datas.illustration.position=='top'){
+        		domelem.append([illus,uicontents]);
+        	}else if(datas.illustration.position=='bottom'){
+        		domelem.append([uicontents,illus]);
+        	}else if(datas.illustration.position=='right'){
+        		domelem.append([$('<div class="col-md-8">').append(uicontents),$('<div class="col-md-4">').append(illus)]);
+        	}else if(datas.illustration.position=='left'){
+        		domelem.append([$('<div class="col-md-4">').append(illus),$('<div class="col-md-8">').append(uicontents)]);
+        	}
+        }else{
+        	domelem.append(uicontents);
+        }
         bindUiEvents();
     };
 
@@ -59,13 +73,17 @@ var rpndropdownmodule = function() {
     
     var validate = function(){
         state=_.map($('select',domelem),function(ele,idx){return $(ele).val()});
-        rpnsequence.handleEndOfModule(state);
+        return state;
     };
     
     var score = function(sol){
         var score=0;
         _.each(sol,function(s,idx){
-            score+=(_.contains(s.alternative,state[idx] )?1:0);
+            if(s.alternative){
+                score += (_.contains(s.alternative,state[idx] ) ? 1 : 0);
+            }else{
+                score += state[idx] == s ? 1 : 0;
+            }
         });
         return score;
     };
