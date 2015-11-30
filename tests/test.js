52646db3a68050e3ecf33a950d3a294d39d535ed
roquierm <michel.roquier@rpn.ch>
2015-11-30 21:20:25 +0000

    add jquery-ui.js and test.js from master

--- tests/test.js
+++ tests/test.js
@@ -1,30 +1,59 @@
+less = {
+    async: true
+};
 require.config({
     "baseUrl": "../../js",
     "shim" : {
         "bootstrap" : { "deps" :['jquery'] },
-        "ext/jquery-sortable":{ "deps" :['jquery'] },
-        "ext/rangyinputs-jquery-src":{ "deps" :['jquery'] },
-        "ext/jquery-disableSelection":{ "deps" :['jquery'] },
+        "ext/jquery-ui": { "deps" :['jquery'] },
         "ext/jquery.maphilight":{ "deps" :['jquery'] },
-        "ext/fabric":{ "deps" :['jquery'] },
-        //"ext/MathJax/mathjax":{ "deps" :['jquery'] }
+        "ext/jquery-disableSelection":{ "deps" :['jquery'] }
     },
     
     "paths": {
-        "jquery": "https://code.jquery.com/jquery-2.1.1",
-        "bootstrap":"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min",
+        "jquery": "https://code.jquery.com/jquery-2.1.3",
+        "bootstrap":"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min",
         "swfobject":"//cdnjs.cloudflare.com/ajax/libs/swfobject/2.2/swfobject",
         "underscore":"//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore",
         "less":"//cdnjs.cloudflare.com/ajax/libs/less.js/2.1.0/less.min",
-        "mathjax":"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMM"
+        "fabric":"//cdnjs.cloudflare.com/ajax/libs/fabric.js/1.5.0/fabric.min"
     }
 });
 
-require(['jquery','bootstrap','swfobject','underscore','less','mathjax','ext/jquery-sortable','ext/rangyinputs-jquery-src','ext/jquery-disableSelection','ext/jquery.maphilight','ext/fabric','ext/MathJax/mathjax','rpnsequence','rpnblackbox','rpncardmaze','rpnclock','rpndragdropsorting','rpngapfull','rpngapsimple','rpnmarker','rpnmqc','rpndropdown','rpnsorting','rpntwolists'], function($) {
+require([
+    'jquery',
+    'bootstrap',
+    'swfobject',
+    'underscore',
+    'less',
+    'fabric',
+    'ext/jquery-disableSelection',
+    'ext/jquery.maphilight',
+    'ext/jquery-ui',
+    'rpnsequence',
+    'rpnblackbox',
+    'rpncardmaze',
+    'rpnclock',
+    'rpndragdropsorting',
+    'rpngapfull',
+    'rpngapsimple',
+    'rpnmarker',
+    'rpnmqc',
+    'rpndropdown',
+    'rpnsorting',
+    'rpntwolists'
+    ], function($) {
     $(document).ready(function(){
         rpnsequence.init({
             debug:true,
-            disablestateloading:true,
+            disablestateloading:false,
+            onmoduleend:function(states,nextstep){
+                console.log('ok');
+                nextstep();
+            },
+            onsequenceend:function(states,score){
+                window.location.href='../';
+            },
             mediapathformatter:function(url){
                 return '../../tests/medias/'+url;
             },
